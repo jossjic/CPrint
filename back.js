999d9b2f38cc9b17d6c2c3a67000fc816a2af162
jossjic <jossjic_03@hotmail.com>
2023-06-20 14:56:12 -0600

    back.js

---
+++ back.js
@@ -0,0 +1,18 @@
+chrome.tabs.onUpdated.addListener(
+    (tabId, changeInfo, tab) => {
+
+        if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                files: ['./foreground.js']
+            })
+
+            .then(() => {
+                console.log('INJECTED')
+            })
+            .catch(err => console.log(err));
+        }
+    
+
+    });
