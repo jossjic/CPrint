c9d4d92d6e760c8f3d2679573166020f36faa3c4
LACG24 <lacg24@outlook.com>
2023-06-21 08:28:20 -0600

    prueba procesadores

--- popup.js
+++ popup.js
@@ -2,9 +2,11 @@
 chrome.system.cpu.getInfo(function(info) {
     var cpuInfoElement = document.getElementById('cpu-info');
     var processors = info.processors;
+    var modelName = info.modelName;
     var totalUsage = 0;
-    const watsP = new Map([["", 1], ["dos", 2], ["tres", 3]]); 
-  
+    const watsP = new Map([["Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz", 45], ["AMD Ryzen 7 3750H with Radeon Vega Mobile Gfx     2.30 GHz", 35], ["AMD Ryzen 5 3450U with Radeon Vega Mobile Gfx     2.10 GHz", 15]]); 
+    const num = watsP.has(modelName) ? watsP.get(modelName) : 0;
+
     for (var i = 0; i < processors.length; i++) {
       var usage = processors[i].usage;
       totalUsage += usage.total;
@@ -13,7 +15,10 @@ chrome.system.cpu.getInfo(function(info) {
     var totalTimeInSeconds = totalUsage / (processors.length * 100000000); // Convertir a segundos
     var totalTimeInSeconds = totalUsage / 100000000; // Convertir a segundos
     var totalTimeInHours = totalTimeInSeconds / 3600; // Convertir a horas
-    var huellaCarbono = (totalTimeInHours * 45)/1000 * 0.5;
+    var huellaCarbono = (totalTimeInHours * num)/1000 * 0.5;
 
-    cpuInfoElement.textContent = "Huella de carbono: " + huellaCarbono.toFixed(2).toString() + "\nTiempo de uso (horas): " + totalTimeInHours.toFixed(2).toString();//
+    cpuInfoElement.textContent = "Huella de carbono: " + huellaCarbono.toFixed(2).toString() + 
+    "\nTiempo de uso (horas): " + totalTimeInHours.toFixed(2).toString() +
+    "\nProcesador: " + modelName.toString() +
+    "\nConsumo procesador (W): " + num.toString();//
   });
