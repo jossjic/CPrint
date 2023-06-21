7d1d514406650bf9ddc6f134810ea0cc9e99760a
LACG24 <lacg24@outlook.com>
2023-06-21 00:07:40 -0600

    prueba de tiempo

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
-chrome.system.cpu.getInfo(function(info) {
+
+/*chrome.system.cpu.getInfo(function(info) {
     var cpuInfoElement = document.getElementById('cpu-info');
     
     var archName = info.archName;
@@ -8,13 +9,34 @@ chrome.system.cpu.getInfo(function(info) {
         totalSum += info.processors[i].usage.total;
     }
 
-
+    //cpuInfoElement.textContent = JSON.stringify(info, null, 2);
     // Mostrar solo los dos elementos específicos en el elemento 'cpu-info'
+
     var cpuInfo = {
       archName: archName,
       modelName: modelName,
       totalSum: totalSum
     };
 
-    cpuInfoElement.textContent = JSON.stringify(cpuInfo, null, 2);
+    //cpuInfoElement.textContent = JSON.stringify(cpuInfo, null, 2);
+    //cpuInfoElement.textContent = totalSum.toString();
+});
+*/
+
+chrome.system.cpu.getInfo(function(info) {
+    var cpuInfoElement = document.getElementById('cpu-info');
+
+    var processors = info.processors;
+    var totalUsage = 0;
+  
+    for (var i = 0; i < processors.length; i++) {
+      var usage = processors[i].usage;
+      totalUsage += usage.total;
+    }
+  
+    var totalTimeInSeconds = totalUsage / (processors.length * 100000000); // Convertir a segundos
+    var totalTimeInSeconds = totalUsage / 100000000; // Convertir a segundos
+  var totalTimeInHours = totalTimeInSeconds / 3600; // Convertir a horas
+
+    cpuInfoElement.textContent = totalTimeInHours.toString();// Imprimir el tiempo de uso del procesador en segundos
   });
