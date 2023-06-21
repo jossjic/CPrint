c845a6548e81ae131a3c6c84360af523990dcb9a
LACG24 <lacg24@outlook.com>
2023-06-20 22:23:01 -0600

    variables separadas

--- popup.js
+++ popup.js
@@ -1,4 +1,20 @@
 chrome.system.cpu.getInfo(function(info) {
     var cpuInfoElement = document.getElementById('cpu-info');
-    cpuInfoElement.textContent = JSON.stringify(info, null, 2);
+    
+    var archName = info.archName;
+    var modelName = info.modelName;
+    var totalSum = 0;
+    for (var i = 0; i < info.processors.length; i++) {
+        totalSum += info.processors[i].usage.total;
+    }
+
+
+    // Mostrar solo los dos elementos específicos en el elemento 'cpu-info'
+    var cpuInfo = {
+      archName: archName,
+      modelName: modelName,
+      totalSum: totalSum
+    };
+
+    cpuInfoElement.textContent = JSON.stringify(cpuInfo, null, 2);
   });
